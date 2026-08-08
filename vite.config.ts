1321766bc0fba071208a7bceddb14788dcc6825e
samiyantannaz1 <samiyantannaz1@gmail.com>
2026-08-08 20:10:29 +0330

    Update K.B.N website

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'node:path'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+// import path from 'node:path'
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-})
+import { defineConfig } from "vite";
+ import react from "@vitejs/plugin-react"; 
+ import tailwindcss from "@tailwindcss/vite"; 
+ export default defineConfig({ 
+  base: "/kbn-fabric/",
+   plugins: [ react(), tailwindcss(), ], });
